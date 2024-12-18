82235721944003c1895a582440804c9c6399b074
David Joy <davjoy@gmail.com>
2024-12-18 14:43:14 -0500

    refactor: migration step: converting .eslintrc.js to eslint.config.js

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-const { createConfig } = require('@openedx/frontend-base/config');
-
-module.exports = createConfig('lint', {
-  parserOptions: {
-    project: path.resolve(__dirname, './tsconfig.json'),
-  },
-});
